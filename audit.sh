6e3d0a3688482f5c1bf9fda08d3f158ab6f6c66b
Kristian Decker <kris@codecker.com>
2021-08-31 10:14:35 -0400

    Completed 2.1.2 and 2.2.1 audit items

--- audit.sh
+++ audit.sh
@@ -1,7 +1,11 @@
 #!/bin/zsh
-
+#
 # Mac OS 11 CIS Benchmark Audit - Level 1
+# Created by Kristian Decker
 # Run Audit as root to minimize permissions issues. Run $ sudo su
+################################################################################################################
+
+# Audit Setup
 
 # Audit date
 TIMESTAMP=$(date +%F)
@@ -18,8 +22,13 @@ echo "Mac OS CIS Benchmark ${TIMESTAMP} \n" >> $REPORT
 # List of all users. Removed all users starting with _, and daemon, nobody, and root users from list
 USERS=$(dscl . list /Users | grep -v -e '_' -e 'root' -e 'nobody' -e 'daemon')
 
+
+################################################################################################################
+
+
 # Section 1
 echo "SECTION 1 Install Updates, Patches and Additional Security Software \n" >> $REPORT
+
 
 # 1.1 Verify all Apple-provided software is current 
 # TODO: Add date check to ensure the response is within the last 30 days
@@ -37,6 +46,7 @@ if [ $AUDIT1_2 = 1 ];
   else echo "FAIL - DISABLED \n" >> $REPORT
 fi
 
+
 # 1.3 Enable Download new updates when available
 echo "1.3 Enable Download new updates when available" >> $REPORT
 AUDIT1_3=$(defaults read /Library/Preferences/com.apple.SoftwareUpdate AutomaticDownload)
@@ -45,6 +55,7 @@ if [ $AUDIT1_3 = 1 ];
   then echo "PASS - ENABLED \n" >> $REPORT
   else echo "FAIL - DISABLED \n" >> $REPORT
 fi
+
 
 # 1.4 Enable app update installs
 echo "1.4 Enable app update installs" >> $REPORT
@@ -55,21 +66,24 @@ if [ $AUDIT1_4 = 1 ];
   else echo "FAIL - DISABLED \n" >> $REPORT
 fi
 
+
 # 1.5 Enable system data files and security updates install
 echo "1.5 Enable system data files and security updates install" >> $REPORT
+# First check
 AUDIT1_5a=$(defaults read /Library/Preferences/com.apple.SoftwareUpdate ConfigDataInstall)
 echo "ConfigDataInstall" >> $REPORT
 if [ $AUDIT1_5a = 1 ];
   then echo "PASS - ENABLED" >> $REPORT
   else echo "FAIL - DISABLED" >> $REPORT
 fi
-
+# Second Check
 AUDIT1_5b=$(defaults read /Library/Preferences/com.apple.SoftwareUpdate CriticalUpdateInstall)
 echo "CriticalUpdateInstall" >> $REPORT
 if [ $AUDIT1_5b = 1 ];
   then echo "PASS - ENABLED \n" >> $REPORT
   else echo "FAIL - DISABLED \n" >> $REPORT
 fi
+
 
 # 1.6 Enable macOS update installs
 echo "1.6 Enable macOS update installs" >> $REPORT
@@ -80,8 +94,13 @@ if [ $AUDIT1_6 = 1 ];
   else echo "FAIL - DISABLED \n" >> $REPORT
 fi
 
+
+################################################################################################################
+
+
 # Section 2
 echo "SECTION 2 System Preferences \n" >> $REPORT
+
 
 # 2.1.1 Turn off Bluetooth, if no paired devices exist
 echo "2.1.1 Turn off Bluetooth, if no paired devices exist" >> $REPORT
@@ -108,3 +127,21 @@ fi
 
 # 2.1.2 Show Bluetooth status in menu bar
 echo "2.1.2 Show Bluetooth status in menu bar" >> $REPORT
+echo "User accounts and Status" >> $REPORT
+for i in $USERS
+  do
+    AUDIT2_1_2=$(sudo -u $i defaults -currentHost read com.apple.controlcenter.plist Bluetooth)
+    if [ $AUDIT2_1_2 = 18 ]
+      then echo $i " PASS - ENABLED" >> $REPORT
+      else echo $i " FAIL - DISABLED" >> $REPORT
+    fi
+  done
+echo "\n" >> $REPORT
+
+# 2.2.1 Enable "Set time and date automatically"
+echo "2.2.1 Enable Set time and date automatically" >> $REPORT
+AUDIT2_2_1=$(systemsetup -getusingnetworktime)
+if [[ $AUDIT2_2_1 =~ "On" ]]
+  then echo "PASS - ENABLED \n" >> $REPORT
+  else echo "FAIL - DISABLED \n" >> $REPORT
+fi
